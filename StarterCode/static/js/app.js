88319def2c86ff79f3eaf9ea4e269b5b7d974df6
Chuck McManus <107281140+mcmanuscw@users.noreply.github.com>
2022-10-02 13:20:00 -0400

    set up data for bar chart; trace and xy values; orientation has to be landscape; find excercise in day 2; exercise 9? top ten example...

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,17 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-data = d3.json(url)
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log("data",data);
+  });
+
+
+//Trace for the sample data / orientation will be landscape
+let trace1 = {
+ //x = otu_ids
+ 
+
+ //y = sample values
+    
+
+}
